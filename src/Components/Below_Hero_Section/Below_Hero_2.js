72386f199d2c59c71f4a0ecbf2eaf5edfffa34c2
WajidAhmed-25 <wajid.g20504@iqra.edu.pk>
2024-09-25 16:04:39 +0500

    data

--- src/Components/Below_Hero_Section/Below_Hero_2.js
+++ src/Components/Below_Hero_Section/Below_Hero_2.js
@@ -10,11 +10,11 @@ const Below_Hero_2 = () => {
 
 
   return (
-    <div>
+    <div className='overflow-x-hidden'>
 
 
       <div className='flex items-center justify-center w-full p-8 pt-12 pb-4 '>
-        <h1 className="relative text-2xl pb-4 font-extrabold  text-center mb-4 text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl custom-underline bg-gradient-to-br from-gray-500 via-[#0b1056] to-gray-500 bg-clip-text">
+        <h1 className="relative text-3xl pb-4 font-extrabold  text-center mb-4 text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl custom-underline bg-gradient-to-br from-gray-500 via-[#0b1056] to-gray-500 bg-clip-text">
           Disability Housing
           <span className="absolute left-1/2 transform -translate-x-1/2 -bottom-2 h-1 w-[100%] mt-6 bg-[#0b1056]"></span>
 
@@ -23,8 +23,8 @@ const Below_Hero_2 = () => {
 
       <div className='flex justify-center text-white '>
 
-        <div className="relative md:w-[40%] w-[90%] p-6  bg-[#0b1056] rounded-3xl -rotate-3 mt-16">
-          <div className="relative w-full p-8 shadow-lg rotate-3 bg-gradient-to-r rounded-3xl border-white/40 border-2 from-[#28317d] to-[#0b1056]rounded-3xl">
+        <div className="relative md:w-[40%] w-[90%] p-6  bg-[#0b1056] rounded-3xl -rotate-3  mt-16">
+          <div className="relative w-full p-8 shadow-lg rotate-3  bg-gradient-to-r rounded-3xl border-white/40 border-2 from-[#28317d] to-[#0b1056]rounded-3xl">
 
 
             <p className="mt-6 mb-4 font-semibold leading-7 tracking-wide text-center text-white text-md">Sydney 24/7 Care provide premium STA and MTAs for individuals
@@ -43,7 +43,7 @@ const Below_Hero_2 = () => {
 
 
       <div className='flex flex-col items-center justify-center w-full p-8 pt-12 pb-8 '>
-        <h1 className="relative text-2xl pb-4 font-extrabold  text-center mb-4 text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl custom-underline bg-gradient-to-br from-gray-500 via-[#0b1056] to-gray-500 bg-clip-text">
+        <h1 className="relative text-3xl pb-4 font-extrabold  text-center mb-4 text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl custom-underline bg-gradient-to-br from-gray-500 via-[#0b1056] to-gray-500 bg-clip-text">
           SIL
           <span className="absolute left-1/2 transform -translate-x-1/2 -bottom-2 h-1 w-[100%] mt-6 bg-[#0b1056]"></span>
         </h1>
@@ -54,8 +54,8 @@ const Below_Hero_2 = () => {
 
       <div className='flex justify-center text-white '>
 
-        <div className="relative md:w-[40%] w-[90%] p-6  bg-[#0b1056] rounded-3xl -rotate-3 mt-8">
-          <div className="relative w-full p-8 shadow-lg rotate-3 bg-gradient-to-r rounded-3xl border-white/40 border-2 from-[#28317d] to-[#0b1056]rounded-3xl">
+        <div className="relative md:w-[40%] w-[90%] p-6  bg-[#0b1056] rounded-3xl md:-rotate-3 rotate-0 mt-8">
+          <div className="relative w-full p-6 shadow-lg md:rotate-3 rotate-0 bg-gradient-to-r  border-white/40 border-2 from-[#28317d] to-[#0b1056] rounded-3xl">
 
             <p className="mt-6 mb-4 font-semibold leading-7 tracking-wide text-center text-white text-md">
               Sydney 24/7 Care provide premium STA and MTAs for individual Supported Independent Living (SIL) helps individuals with disabilities or chronic illnesses live independently while receiving support for daily tasks like cooking, cleaning, and social engagement.<br /><br /> To access SIL, you need an OT assessment to determine eligibility, after which your support coordinator will connect you with a suitable SIL provider. The provider will create a quote based on your needs, which is submitted to the NDIS for approval. Once the NDIS reviews the evidence and finds it reasonable, your SIL journey can begin. At Sydney 24/7 Care, we offer comprehensive SIL services, including assistance with feeding, medication, and daily living.
@@ -74,7 +74,7 @@ const Below_Hero_2 = () => {
 
 
       <div className='flex flex-col items-center justify-center w-full p-8 pt-12 pb-8 '>
-        <h1 className="relative text-2xl pb-4 font-extrabold text-center mb-4 text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl custom-underline bg-gradient-to-br from-gray-500 via-[#0b1056] to-gray-500 bg-clip-text">
+        <h1 className="relative text-3xl pb-4 font-extrabold text-center mb-4 text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl custom-underline bg-gradient-to-br from-gray-500 via-[#0b1056] to-gray-500 bg-clip-text">
           STA
           <span className="absolute left-1/2 transform -translate-x-1/2 -bottom-2 h-1 w-[100%] mt-6 bg-[#0b1056]"></span>
         </h1>
@@ -109,7 +109,7 @@ const Below_Hero_2 = () => {
 
 
       <div className='flex flex-col items-center justify-center w-full p-8 pt-12 pb-8 '>
-        <h1 className="relative text-2xl pb-4 font-extrabold  text-center mb-4 text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl custom-underline bg-gradient-to-br from-gray-500 via-[#0b1056] to-gray-500 bg-clip-text">
+        <h1 className="relative text-3xl pb-4 font-extrabold  text-center mb-4 text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl custom-underline bg-gradient-to-br from-gray-500 via-[#0b1056] to-gray-500 bg-clip-text">
           MTA
           <span className="absolute left-1/2 transform -translate-x-1/2 -bottom-2 h-1 w-[100%] mt-6 bg-[#0b1056]"></span>
         </h1>
@@ -120,8 +120,8 @@ const Below_Hero_2 = () => {
 
 
       <div className='flex justify-center text-white '>
-        <div className="relative md:w-[40%] w-[90%] p-6  bg-[#0b1056] rounded-3xl -rotate-3 mt-8">
-          <div className="relative w-full p-8 shadow-lg rotate-3 bg-gradient-to-r rounded-3xl border-white/40 border-2 from-[#28317d] to-[#0b1056]rounded-3xl">
+        <div className="relative md:w-[40%] w-[90%] p-6  bg-[#0b1056] rounded-3xl md:-rotate-3 rotate-0 mt-8">
+          <div className="relative w-full p-8 shadow-lg md:rotate-3 rotate-0 bg-gradient-to-r rounded-3xl border-white/40 border-2 from-[#28317d] to-[#0b1056]rounded-3xl">
 
             <p className="mt-6 mb-4 font-semibold leading-7 tracking-wide text-center text-white text-md">
               If you cannot move into a long-term home right now, NDIS will finance you for medium-term accommodation support.
